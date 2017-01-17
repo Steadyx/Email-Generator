619c542a22336e79b9a6d0efc2ad6c72f74aecaf
Edward Kemp <edward.kemp@stoneshot.com>
2017-01-17 09:31:56 +0000

    made some style changes / removed red background color on divs in outlook / updated some HTML

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,114 +1,122 @@
-var gulp = require( 'gulp' );
-var path = require( 'path' );
-var zip = require( 'gulp-zip' );
-var pug = require( 'gulp-pug' );
-var sass = require( 'gulp-sass' );
-var inlineCss = require( 'gulp-inline-css' );
-var neat = require( 'node-neat' )
-  .includePaths;
-var browserSync = require( 'browser-sync' )
-  .create(),
-  reload = browserSync.reload;
+var gulp = require('gulp');
+var path = require('path');
+var zip = require('gulp-zip');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var imageMin = require('gulp-imagemin')
+var inlineCss = require('gulp-inline-css');
+var neat = require('node-neat')
+	.includePaths;
+var browserSync = require('browser-sync')
+	.create(),
+	reload = browserSync.reload;
 
 var path = {
-  html: './src/*.html',
-  htmlFiles: './**/*.html',
-  buildHTML: './src/build/*.html',
-  buildCSS: './src/build/css/*.css',
-  sass: './src/styles/**/*.scss',
-  pug: './src/**/*.pug'
-}
 
-gulp.task( 'serve', function() {
+	html: './src/*.html',
+	allHTML: './**/*.html',
+	buildHTML: './src/build/*.html'
+	pug: './src/**/*.pug'
+	buildCSS: './src/build/css/*.css',
+	sass: './src/styles/**/*.scss'
+	buildPath: './src/build'
 
-  browserSync.init( {
-    proxy: 'localhost:3000/build'
-  } )
+};
 
-  gulp.watch( path.sass, [ 'sass' ] );
+gulp.task('serve', function () {
 
-} );
+	browserSync.init({
+		proxy: 'localhost:3000/build'
+	})
 
-gulp.task( 'inlineHTML', function() {
-  gulp
-    .src( path.html )
-    .pipe( inlineCss( {
-      preserveMediaQueries: true,
-      applyStyleTags: true,
-      applyLinkTags: true,
-      removeStyleTags: false,
-      removeLinkTags: false,
-      applyTableAttributes: false,
-      removeHtmlSelectors: false,
-      applyWidthAttributes: false
-    } ) )
-    .pipe( gulp.dest( './src/build' ) )
-    .pipe( browserSync.stream() );
+	gulp.watch(path.sass, ['sass']);
 
-} );
+});
 
-gulp.task( 'zip', function() {
+gulp.task('inlineHTML', function () {
+	gulp
+		.src(path.html)
+		.pipe(inlineCss({
+			preserveMediaQueries: true,
+			applyStyleTags: true,
+			applyLinkTags: true,
+			removeStyleTags: false,
+			removeLinkTags: false,
+			applyTableAttributes: false,
+			removeHtmlSelectors: false,
+			applyWidthAttributes: false
+		}))
+		.pipe(gulp.dest(path))
+		.pipe(browserSync.stream());
 
-  return gulp
-    .src( './src/build/**' )
-    .pipe( zip( 'build.zip' ) )
-    .pipe( browserSync.stream() )
-    .pipe( gulp.dest( 'production' ) );
-} );
+});
 
-gulp.task( 'sass', function() {
+gulp.task('zip', function () {
 
-  return gulp
-    .src( path.sass )
-    .pipe( sass( {
-      includePaths: [ 'styles' ].concat( neat )
-    } ) )
-    .pipe( gulp.dest( './src/css' ) )
-    .pipe( browserSync.stream() )
-    .pipe( gulp.dest( './src/build/css' ) )
-    .on( 'error', sass.logError );
+	return gulp
+		.src('./src/build/**')
+		.pipe(zip('build.zip'))
+		.pipe(browserSync.stream())
+		.pipe(gulp.dest('production'));
+});
 
-  gulp
-    .watch( path.html )
-    .on( 'change', reload );
-} );
+gulp.task('sass', function () {
 
-function complete( file ) {
+	return gulp
+		.src(path.sass)
+		.pipe(sass({
+			includePaths: ['styles'].concat(neat)
+		}))
+		.pipe(gulp.dest('./src/css'))
+		.pipe(browserSync.stream())
+		.pipe(gulp.dest('./src/build/css'))
+		.on('error', sass.logError);
 
-  var folder = path
-    .basename( file.path )
-    .replace( /\..*html/, '/' );
+	gulp
+		.watch(path.html)
+		.on('change', reload);
+});
 
-  return './build/templates' + folder;
+function complete(file) {
+
+	var folder = path
+		.basename(file.path)
+		.replace(/\..*html/, '/');
+
+	return './build/templates' + folder;
 
 }
 
 gulp
-  .task( 'pug', function() {
+	.task('pug', function () {
 
-    gulp
-      .src( path.pug )
-      .pipe( pug() )
-      .pipe( gulp.dest( './build/*.html' ) )
-      .on( 'done', complete );
+		gulp
+			.src(path.pug)
+			.pipe(pug())
+			.pipe(gulp.dest('./build/*.html'))
+			.on('done', complete);
 
-  } )
+	});
 
-gulp.task( 'watch', function() {
+gulp.tast('minifyImages', function () {
+	return gulp.src()
+})
 
-  gulp.watch( path.html, [ 'inlineHTML' ] );
-  gulp.watch( path.buildCSS, [ 'inlineHTML' ] );
-  gulp.watch( path.buildCSS, [ 'zip' ] );
-  gulp.watch( path.buildHTML, [ 'inlineHTML' ] );
-  gulp.watch( path.buildHTML, [ 'zip' ] );
-  gulp.watch( path.pug, [ 'pug' ] );
+gulp.task('watch', function () {
 
-} );
+	gulp.watch(path.html, ['inlineHTML']);
+	gulp.watch(path.buildCSS, ['inlineHTML']);
+	gulp.watch(path.buildCSS, ['zip']);
+	gulp.watch(path.buildHTML, ['inlineHTML']);
+	gulp.watch(path.buildHTML, ['zip']);
+	// gulp.watch( path.pug, [ 'pug' ] );
 
-gulp.task( 'default', [
+});
+
+gulp.task('default', [
   'serve', 'watch', 'zip', 'sass'
-], function() {
+], function () {
 
-  gulp.start( 'sass' );
+	gulp.start('sass');
 
-} );
+});
