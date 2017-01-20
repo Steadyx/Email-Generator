a2d6c96c891cf4d5aaa54dbd79f6a4c8da0769f3
Steadyx <edwardlanekemp@gmail.com>
2017-01-20 18:01:14 +0000

    made some changes to index.js - added a new option

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,85 +4,123 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = Generator.extend({
-  prompting: function () {
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the ' + chalk.red('generator-email') + ' generator!'));
+	prompting: function () {
+		// Have Yeoman greet the user.
+		this.log(yosay('Welcome to the ' + chalk.yellow('easyemail') + ' generator!'));
 
-    var prompts = [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Whats the name of your project going to be?',
-        default: 'Resonsive-Email'
-      }
+		var prompts = [
+			{
+				type: 'input',
+				name: 'name',
+				message: 'Whats the name of your project going to be?',
+				default: this.appname
+      }, {
+				type: 'list',
+				name: 'styles',
+				message: 'What CSS framework would you like to use?',
+				choices: [{
+					name: 'CSS',
+					value: 'includeCss'
+				}, {
+					name: 'Sass',
+					value: 'includeSass',
+				}, {
+					name: 'Less',
+					value: 'includeLess',
+				}, {
+					name: 'Scss',
+					value: 'includeScss',
+				}]
+			}
     ];
 
-    return this
-      .prompt(prompts)
-      .then(function (props) {
-        // To access props later use this.props.someAnswer;
-        this.props = props;
-      }.bind(this));
-  },
+		return this.prompt(prompts)
+			.then(function (answers) {
+				// To access users configs later use this.answers.prop;
+				this.answers = answers;
 
-  // Application Logic
+				this.log(this.answers.styles)
 
-  config: function () {
-    // Config Files
-    this
-      .fs
-      .copyTpl(this.templatePath('_package.json'), this.destinationPath('package.json'), {name: this.props.name});
-    this
-      .fs
-      .copyTpl(this.templatePath('_.gitignore'), this.destinationPath('.gitignore'));
-  },
-  // Application Files
-  app: function () {
-    this
-      .fs
-      .copyTpl(this.templatePath('_index.js'), this.destinationPath('index.js'));
-    this
-      .fs
-      .copyTpl(this.templatePath('_gulpfile.js'), this.destinationPath('gulpfile.js'));
+			}.bind(this));
+	},
 
-    // HTML
+	// Application Logic
 
-    this
-      .fs
-      .copyTpl(this.templatePath('_src/_index.html'), this.destinationPath('src/index.html'));
-    // CSS Framework - BASE
-    this
-      .fs
-      .copyTpl(this.templatePath('_src/_styles/_main.scss'), this.destinationPath('src/styles/main.scss'));
-    this
-      .fs
-      .copyTpl(this.templatePath('_src/_styles/_framework/_base/_font-styles.scss'), this.destinationPath('src/styles/framework/base/_font-styles.scss'));
-    this
-      .fs
-      .copyTpl(this.templatePath('_src/_styles/_framework/_base/_global-styles.scss'), this.destinationPath('src/styles/framework/base/_global-styles.scss'));
-    this
-      .fs
-      .copyTpl(this.templatePath('_src/_styles/_framework/_base/_layout.scss'), this.destinationPath('src/styles/framework/base/_layout.scss'));
-    this
-      .fs
-      .copyTpl(this.templatePath('_src/_styles/_framework/_base/_responsive.scss'), this.destinationPath('src/styles/framework/base/_responsive.scss'));
+	config: function () {
+		// Config Files
+		this
+			.fs
+			.copyTpl(this.templatePath('_package.json'), this.destinationPath('package.json'), {
+				name: this.answers.name
+			});
+		this
+			.fs
+			.copyTpl(this.templatePath('_.gitignore'), this.destinationPath('.gitignore'));
+	},
+	// Application Files
+	app: function () {
 
-    // CSS Framework - MODULES
 
-    this
-      .fs
-      .copyTpl(this.templatePath('_src/_styles/_framework/_modules/_colors.scss'), this.destinationPath('src/styles/framework/modules/_colors.scss'));
-    this
-      .fs
-      .copyTpl(this.templatePath('_src/_styles/_framework/_partials/_footer.scss'), this.destinationPath('src/styles/framework/partials/_footer.scss'));
-    this
-      .fs
-      .copyTpl(this.templatePath('_src/_styles/_framework/_partials/_header.scss'), this.destinationPath('src/styles/framework/partials/_header.scss'));
-    this
-      .fs
-      .copyTpl(this.templatePath('_src/_styles/_framework/_partials/_mid-section.scss'), this.destinationPath('src/styles/framework/partials/_mid-section.scss'));
-  },
-  install: function () {
-    this.installDependencies();
-  }
+		this
+			.fs
+			.copy(this.templatePath('_index.js'), this.destinationPath('index.js'));
+
+		// HTML
+
+		this.fs
+			.copyTpl(this.templatePath('_src/_index.html'), this.destinationPath('src/index.html'), {
+				name: this.answers.name
+			});
+
+		this.fs.copy(this.templatePath('_gulpfile.js'), this.destinationPath('gulpfile.js'));
+
+		// CSS Framework - BASE
+		if (this.answers.styles === 'includeScss') {
+
+
+			this.fs.copyTpl(this.templatePath('_src/_styles/_framework/_**/_*.scss'), this.destinationPath('_src/_styles/_framework/_**/_*.scss'))
+
+			this
+				.fs
+				.copyTpl(this.templatePath('_src/_styles/_main.scss'), this.destinationPath('src/styles/main.scss'));
+			this
+				.fs
+				.copyTpl(this.templatePath('_src/_styles/_framework/_base/_font-styles.scss'), this.destinationPath('src/styles/framework/base/_font-styles.scss'));
+			this
+				.fs
+				.copyTpl(this.templatePath('_src/_styles/_framework/_base/_global-styles.scss'), this.destinationPath('src/styles/framework/base/_global-styles.scss'));
+			this
+				.fs
+				.copyTpl(this.templatePath('_src/_styles/_framework/_base/_layout.scss'), this.destinationPath('src/styles/framework/base/_layout.scss'));
+			this
+				.fs
+				.copyTpl(this.templatePath('_src/_styles/_framework/_base/_responsive.scss'), this.destinationPath('src/styles/framework/base/_responsive.scss'));
+
+			// CSS Framework - MODULES
+
+			this
+				.fs
+				.copyTpl(this.templatePath('_src/_styles/_framework/_modules/_colors.scss'), this.destinationPath('src/styles/framework/modules/_colors.scss'));
+			this
+				.fs
+				.copyTpl(this.templatePath('_src/_styles/_framework/_partials/_footer.scss'), this.destinationPath('src/styles/framework/partials/_footer.scss'));
+			this
+				.fs
+				.copyTpl(this.templatePath('_src/_styles/_framework/_partials/_header.scss'), this.destinationPath('src/styles/framework/partials/_header.scss'));
+			this
+				.fs
+				.copyTpl(this.templatePath('_src/_styles/_framework/_partials/_mid-section.scss'), this.destinationPath('src/styles/framework/partials/_mid-section.scss'));
+
+		}
+
+		if (this.answers.styles === 'includeCss') {
+			this
+				.fs
+				.copyTpl(this.templatePath('_src/_styles/_css/_main.css'), this.destinationPath('src/styles/main.css'));
+		}
+
+	},
+	install: function () {
+		this.installDependencies();
+	}
 });
